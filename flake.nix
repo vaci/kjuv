660443832f7e18d537a77b0db769587354ca487e
Vaci <vaci@vaci.org>
2026-01-05 15:51:30 +0000

    use stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "kjuv";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs = { nixpkgs, ...}:
