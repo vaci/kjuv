12f1cb7989c391438f26f2616caf5d3cc6461f51
Vaci <vaci@vaci.org>
2026-01-07 14:15:38 +0000

    set up ccache in dev shell

--- flake.nix
+++ flake.nix
@@ -9,16 +9,17 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      ccache = pkgs.lib.getExe pkgs.ccache;
     in
       {
-        defaultPackage.x86_64-linux =
-          with import nixpkgs { system = "x86_64-linux"; };
-          callPackage ./package.nix { };
+        defaultPackage.${system} =
+          pkgs.callPackage ./package.nix { };
 
         devShells.x86_64-linux.default = pkgs.mkShell {
           buildInputs = [
             pkgs.cmake
             pkgs.capnproto
+            pkgs.ccache
             pkgs.openssl
             pkgs.gcc
             pkgs.libuv.dev
@@ -26,6 +27,11 @@
             pkgs.ninja
             pkgs.pkg-config
           ];
+
+          shellHook = ''
+            export CMAKE_C_COMPILER_LAUNCHER=${ccache}
+            export CMAKE_CXX_COMPILER_LAUNCHER=${ccache}
+          '';
         };
       };
 }
